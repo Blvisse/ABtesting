9327feaf068e1953d048d639c6311f7ee7503309
Blvisse <blaisepke@gmail.com>
2021-07-24 23:30:54 +0300

    Integrated CI testing

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"Test for A/B  testing"
--- tests/test_AB.py
+++ tests/test_AB.py
@@ -7,22 +7,21 @@ if module_path not in sys.path:
     sys.path.append(module_path+"\\scripts")
 
 if module_path not in sys.path:
-    sys.path.append(module_path+"\\models")
+    sys.path.append(module_path+"\\model")
 
-from loadDvc import getData
+from loadDvc import dvcData
+dvcInstance=dvcData()
 
 
-class Tests(unittest.TestCase):
+class TestsAB(unittest.TestCase):
 
-    def __init__ (self):
-        pass
-        
     
-    def checkforPlatform(self):
+    
+    def test_checkforPlatform(self):
         #check to see if the data versions have the same data 
         #platform version shouldn't contain browser column
         #while browser shouldn't contain platform column
-        pdata=getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
+        pdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
         counter=0
         if 'browser' in pdata.columns:
             counter = 1
@@ -34,16 +33,16 @@ class Tests(unittest.TestCase):
             return counter
 
         self.assertEqual(counter , 2)
-    def fullDataset(self):
+    def test_fullDataset(self):
 
-        pdata=getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
+        pdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
         #check to confirm 8 columns
         self.assertEqual(pdata.shape[1],8)
-    def checkforBrowser(self):
+    def test_checkforBrowser(self):
                 #check to see if the data versions have the same data 
         #platform version shouldn't contain browser column
         #while browser shouldn't contain platform column
-        bdata=getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
+        bdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
         counter=0
         if 'platform_os' in bdata.columns:
             counter = 1
@@ -55,20 +54,19 @@ class Tests(unittest.TestCase):
             return counter
 
         self.assertEqual(counter , 2)
-    def checkNulls(self):
+    def test_checkNulls(self):
 
         #select column and check for nulls
-        bdata=getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
+        bdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
         
 
-        self.assertEqual(bdata['broswer'].isnull().sum(),0)
+        self.assertEqual(bdata['experiment'].isnull().sum(),0)
 
 
+   
 
 if __name__ == '__main__':
-	unittest.main()      
-
-
+    unittest.main()
 
 
 
