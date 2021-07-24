826272c5d7e9fb4c1412a6e332ca59a9cf7dff46
Blvisse <blaisepke@gmail.com>
2021-07-25 01:17:16 +0300

    test_AB update

--- tests/test_AB.py
+++ tests/test_AB.py
@@ -14,8 +14,13 @@ sys.path.append(os.path.abspath(os.path.join('../..')))
 
 import dvc.api
 
+data_url=dvc.api.get_url(path='data/AdSmartABdata.csv',repo='https://github.com/Blvisse/ABtesting',rev='cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
+pdata=pd.read_csv(data_url)
 
+data=dvc.api.get_url(path='data/AdSmartABdata.csv',repo='https://github.com/Blvisse/ABtesting',rev='f2ee815f7d230722139cdb43b3817f44a1ce9064')
+bdata=pd.read_csv(data)
 class TestsAB(unittest.TestCase):
+
 
     
     
@@ -23,8 +28,7 @@ class TestsAB(unittest.TestCase):
         #check to see if the data versions have the same data 
         #platform version shouldn't contain browser column
         #while browser shouldn't contain platform column
-        data_url=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
-        pdata=pd.read_csv(data_url)
+                
         # pdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
         counter=0
         if 'browser' in pdata.columns:
@@ -39,8 +43,8 @@ class TestsAB(unittest.TestCase):
         self.assertEqual(counter , 2)
     def test_fullDataset(self):
 
-        data_url=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
-        pdata=pd.read_csv(data_url)
+        # data_url=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
+        # pdata=pd.read_csv(data_url)
         # pdata=dvcInstance.getData('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','cbf35f6e43a99698ba53718ed8a8c8da8f3da722')
         #check to confirm 8 columns
         self.assertEqual(pdata.shape[1],8)
@@ -48,8 +52,7 @@ class TestsAB(unittest.TestCase):
                 #check to see if the data versions have the same data 
         #platform version shouldn't contain browser column
         #while browser shouldn't contain platform column
-        data=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
-        bdata=pd.read_csv(data)
+
         counter=0
         if 'platform_os' in bdata.columns:
             counter = 1
@@ -65,8 +68,8 @@ class TestsAB(unittest.TestCase):
 
         #select column and check for nulls
         #we select the experiment column to ensure everyone recorded was allocated 
-        data=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
-        bdata=pd.read_csv(data)
+        # data=dvc.api.get_url('data/AdSmartABdata.csv','https://github.com/Blvisse/ABtesting','f2ee815f7d230722139cdb43b3817f44a1ce9064')
+        # bdata=pd.read_csv(data)
 
         self.assertEqual(bdata['experiment'].isnull().sum(),0)
 
